2dba241d32e1d5fec2efec23715eef27941b5435
Jason Bugallon <jbugallo@uci.edu>
2026-04-07 17:27:34 -0700

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+class TimeMap:
+
+    # Time: O(1)
+    def __init__(self):
+        self.map = defaultdict(list)
+
+    # Time: O(1)
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.map[key].append( (timestamp, value) )
+
+    # Time: O(logn)
+    def get(self, key: str, timestamp: int) -> str:
+        searchList = self.map[key]
+        l, r = 0, len(searchList) - 1
+
+        mostRecentTimestamp = (0, "")
+
+        while l <= r:
+            m = (l + r) // 2
+
+            midTime, midValue = searchList[m]
+
+            if timestamp == midTime:
+                return midValue
+            elif timestamp > midTime:
+                mostRecentTimestamp = max(mostRecentTimestamp, (midTime, midValue)) 
+                l = m + 1
+            else:
+                r = m - 1
+        
+        return mostRecentTimestamp[1]
