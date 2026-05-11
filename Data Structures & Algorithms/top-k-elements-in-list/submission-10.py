da54d1f15a076397d93423d6cdce37ddbd91acec
Jason Bugallon <jbugallo@uci.edu>
2026-05-12 03:04:12 +0900

    Add: top-k-elements-in-list - submission-10

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-10.py
@@ -0,0 +1,36 @@
+from heapq import heappush, heappop
+from collections import defaultdict
+
+class Solution:
+    # Time: O(nlogk)
+    # Space: O(n)
+    # def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    #     heap = []
+    #     freq = defaultdict(int)
+    #     for n in nums:
+    #         freq[n] += 1
+    #     res = []
+    #     for key in freq:
+    #         heappush(heap, (freq[key], key))
+    #         if len(heap) > k:
+    #             heappop(heap)
+    #     return [node[1] for node in heap]
+
+    # Time: O(n)
+    # Space: O(n)
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = defaultdict(int)
+        for n in nums:
+            freq[n] += 1
+        buckets = [[] for _ in range(max(freq.values())+1)]
+        
+        for key in freq:
+            buckets[freq[key]].append(key)
+        
+        res = []
+        for bucket in range(len(buckets)-1, 0, -1):
+            for n in buckets[bucket]:
+                res.append(n)
+            if len(res) == k:
+                return res
+        return res
