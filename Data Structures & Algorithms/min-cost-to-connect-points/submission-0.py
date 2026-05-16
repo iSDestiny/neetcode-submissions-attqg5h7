a239aae6057582ced9f43a5b70d26b0fcdecd050
Jason Bugallon <jbugallo@uci.edu>
2026-05-17 06:04:26 +0900

    Add: min-cost-to-connect-points - submission-0

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-0.py
@@ -0,0 +1,40 @@
+# Time: O(n^2logn)
+# Space: O(n^2)
+
+from typing import Tuple
+
+class DSU:
+    def __init__(self, points: List[List[int]]):
+        self.parent = {(x,y): (x,y) for x,y in points}
+        self.rank = {(x,y): 0 for x,y in points}
+    
+    def find(self, a: Tuple[int, int]) -> Tuple[int, int]:
+        if self.parent[a] != a:
+            self.parent[a] = self.find(self.parent[a]) # path compression
+        return self.parent[a]
+    
+    def union(self, a: Tuple[int,int], b: Tuple[int, int]) -> bool:
+        ra, rb = self.find(a), self.find(b)
+        if ra == rb:
+            return False
+        if self.rank[ra] < self.rank[rb]:
+            ra, rb = rb, ra
+        self.parent[rb] = ra
+        if self.rank[ra] == self.rank[rb]:
+            self.rank[ra] += 1
+        return True
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        edges = []
+        for xi, yi in points: # O(n^2)
+            for xj, yj in points:
+                if xi == xj and yi == yj: continue
+                edges.append(((xi, yi), (xj, yj), abs(xi-xj) + abs(yi-yj)))
+        edges = sorted(edges, key=lambda x: x[2])
+        dsu = DSU(points)
+        cost = 0
+        for u, v, d in edges:
+            if dsu.union(u, v):
+                cost += d
+        return cost
