ff27efa5369642a51667ae14152e52f3111dc65a
Jason Bugallon <jbugallo@uci.edu>
2026-05-17 17:04:31 +0900

    Add: foreign-dictionary - submission-8

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-8.py
@@ -0,0 +1,47 @@
+from collections import defaultdict, deque
+
+class Solution:
+    def foreignDictionary(self, words: List[str]) -> str:
+        if not words:
+            return ""
+        if len(words) == 1:
+            return words[0]
+
+        # build adj list <- this is the hard part
+        adjList = {c: [] for w in words for c in w}
+        indegrees = {c: 0 for c in adjList}
+
+        for i in range(len(words)-1):
+            word1 = words[i]
+            word2 = words[i+1]
+            
+            if len(word1) > len(word2) and word1.startswith(word2):
+                return ""
+
+
+            if word1 != word2:
+                for ci in range(len(word2)):
+                    if ci >= len(word1):
+                        break
+    
+                    if word1[ci] != word2[ci]:
+                        adjList[word1[ci]].append(word2[ci])
+                        indegrees[word2[ci]] += 1
+                        break
+            
+        
+        # topological sort
+        queue = deque([c for c in indegrees if indegrees[c] == 0])
+        
+        ordering = ""
+        while queue:
+            u = queue.popleft()
+            ordering += u
+            for v in adjList[u]:
+                indegrees[v] -= 1
+                if indegrees[v] == 0:
+                    queue.append(v)
+        
+        if len(ordering) != len(indegrees): # cycle found
+            return ""
+        return ordering
