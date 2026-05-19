f470d2ab112e1ef9d1a375747186a9758d5f8b40
Jason Bugallon <jbugallo@uci.edu>
2026-05-19 10:44:54 +0900

    Add: three-integer-sum - submission-13

---
+++ Data Structures & Algorithms/three-integer-sum/submission-13.py
@@ -0,0 +1,33 @@
+# [-1,0,1,2,-1,-4] -> sorted -> [-4, -1, -1, 0, 1, 2] triplets = [[-1,-1,2]]
+#                                    i   j         k == 0
+# O(nlogn + n^2) -> O(n^2)
+# Space: O(1)
+# constraint:
+# nums.length >= 3
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) < 3:
+            return []
+
+        nums.sort()
+        res = []
+        for i in range(len(nums)-2):
+            # skip duplicates
+            if i > 0 and nums[i-1] == nums[i]:
+                continue
+
+            j,k = i+1, len(nums)-1
+            while j < k:
+                curr_sum = nums[i] + nums[j] + nums[k]
+                if curr_sum == 0:
+                    res.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+                    #skip duplicates
+                    while j < k and nums[j-1] == nums[j]:
+                        j += 1
+                elif curr_sum < 0:
+                    j += 1
+                else:
+                    k -= 1
+        return res
