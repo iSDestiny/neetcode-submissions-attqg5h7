78324f77bc4df336d24ce8110f3d57f4d0ecefe9
Jason Bugallon <jbugallo@uci.edu>
2026-06-25 09:39:06 +0900

    Add: word-break - submission-8

---
+++ Data Structures & Algorithms/word-break/submission-8.py
@@ -0,0 +1,35 @@
+class Solution:
+    # iterate through s recursively:
+    #. - for each word in wordDict check if a word can be formed from current index i, if so recurse on these two choices:
+    #.    - choose to use this word and recurse on the subproblem where i is the position of the rest of the string
+    #.    - skip this word, and choose another word to check for the current index
+    #. - return True if any of the recursive paths above reaches the end
+    # Time: O(n^2*m)
+    # 
+    # neetcode -> code -> "" -> True
+    # applepenapple -> penapple -> apple -> "" -> True
+    # catsincars -> incars -> [cars] -> [s] -> False
+    #            -> sincars -> [cars] -> [s] -> False
+    # def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+    #     cache = [None] * len(s)
+    #     def recurse(i: int) -> bool:
+    #         if i == len(s):
+    #             return True
+    #         if cache[i] != None:
+    #             return cache[i]
+    #         found = False
+    #         for w in wordDict:
+    #             if s[i:i+len(w)] == w:
+    #                 found = found or recurse(i+len(w))
+    #         cache[i] = found
+    #         return cache[i]
+    #     return recurse(0)
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False] * (len(s)+1)
+        dp[len(s)] = True
+        for i in range(len(s)-1,-1,-1):
+            found = False
+            for w in wordDict:
+                if s[i:i+len(w)] == w and dp[i+len(w)]:
+                    dp[i] = True
+        return dp[0]
