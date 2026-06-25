d54054cf7388bada331da46fe6e6fb8a8b8a4d62
Jason Bugallon <jbugallo@uci.edu>
2026-06-25 19:16:36 +0900

    Add: count-connected-components - submission-7

---
+++ Data Structures & Algorithms/count-connected-components/submission-7.py
@@ -0,0 +1,30 @@
+class DSU:
+    def __init__(self, n: int):
+        self.parent = list(range(n))
+        self.rank = [0]*n
+        self.sets = n
+    
+    def find(self, a: int):
+        if self.parent[a] != a:
+            self.parent[a] = self.find(self.parent[a]) # path compression
+        return self.parent[a]
+    
+    def union(self, a: int, b: int) -> bool:
+        ra,rb = self.find(a), self.find(b)
+        if ra == rb: # cycle found
+            return False
+        if self.rank[ra] < self.rank[rb]:
+            ra,rb = rb,ra
+        self.parent[rb] = ra
+        if self.rank[ra] == self.rank[rb]:
+            self.rank[ra] += 1
+        self.sets -= 1
+        return True
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        dsu = DSU(n)
+
+        for u,v in edges:
+            dsu.union(u,v)
+        return dsu.sets
