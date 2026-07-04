0e4ba65235a3721f48863854f0658b2b65f41afc
Jason Bugallon <jbugallo@uci.edu>
2026-07-04 10:30:32 +0900

    Add: longest-increasing-path-in-matrix - submission-3

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-3.py
@@ -0,0 +1,30 @@
+class Solution:
+    # Perform a DFS from every cell (row,col) to get the longest strictly increasing path from this cell
+    # For every cell store the score in a cache to be used for later computation (memoization)
+    # Time: O(n*m)
+    # Space: O(n*m)
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        ROW = len(matrix)
+        COL = len(matrix[0])
+        cache = [[-1]*COL for _ in range(ROW)]
+
+        def dfs(r: int, c: int) -> int:
+            if cache[r][c] != -1:
+                return cache[r][c]
+            directions = [(1, 0), (-1, 0), (0, -1), (0,1)]
+            maxLen = 1
+            for dr, dc in directions:
+                r2,c2 = r+dr, c+dc
+                if r2 < 0 or c2 < 0 or r2 >= ROW or c2 >= COL:
+                    continue
+                if matrix[r2][c2] > matrix[r][c]:
+                    maxLen = max(maxLen, 1+dfs(r2,c2))
+            cache[r][c] = maxLen
+            return cache[r][c]
+        
+        maxLen = 1
+        for r in range(ROW):
+            for c in range(COL):
+                maxLen = max(maxLen, dfs(r,c))
+        return maxLen
+        
