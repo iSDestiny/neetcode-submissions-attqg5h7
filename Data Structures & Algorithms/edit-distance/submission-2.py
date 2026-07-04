13c18279510973783026542a62fba1ec7e06dc99
Jason Bugallon <jbugallo@uci.edu>
2026-07-05 08:31:39 +0900

    Add: edit-distance - submission-2

---
+++ Data Structures & Algorithms/edit-distance/submission-2.py
@@ -0,0 +1,38 @@
+# Brute Force Recursion:
+# - base cases:
+#   - if j == len(word2): return len(word1) - i # If word2 is done but word1 isn't, then delete the rest of word1
+#   - if i == len(word1): return len(word2) - j # If word1 is done but word2 isn't, then insert the rest of missing chars
+# - if word1[i] == word2[j]: recurse(i+1, j+1)
+# - else:
+#   - Insert: recurse(i, j+1)
+#   - Delete: recurse(i+1, j)
+#   - Replace: recurse(i+1, j+1)
+# Time: O(3^n+m)
+# Space: O(n+m)
+#
+# Memoization:
+# Time: O(n*m)
+# Space: O(n*m)
+
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        cache = [[-1]*len(word2) for _ in range(len(word1))]
+
+        def recurse(i: int, j: int) -> int:
+            if j == len(word2): 
+                return len(word1) - i
+            if i == len(word1):
+                return len(word2) - j
+            if cache[i][j] > -1:
+                return cache[i][j]
+            # match
+            if word1[i] == word2[j]:
+                return recurse(i+1,j+1)
+            insert = 1 + recurse(i, j+1) 
+            delete = 1 + recurse(i+1, j)
+            replace = 1 + recurse(i+1, j+1)
+
+            cache[i][j] = min(insert, delete, replace) 
+            return cache[i][j]
+        
+        return recurse(0,0)
