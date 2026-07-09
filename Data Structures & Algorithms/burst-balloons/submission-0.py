81d38bb5046deb884399dd247db7506ebe4403b9
Jason Bugallon <jbugallo@uci.edu>
2026-07-10 05:48:34 +0900

    Add: burst-balloons - submission-0

---
+++ Data Structures & Algorithms/burst-balloons/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        nums = [1] + nums + [1]
+        cache = [[-1]*len(nums) for _ in range(len(nums))]
+
+        def recurse(l: int, r: int) -> int:
+            if l > r:
+                return 0
+            if cache[l][r] != -1:
+                return cache[l][r]
+            cache[l][r] = 0
+            for i in range(l, r+1):
+                current = nums[l-1] * nums[i] * nums[r+1]
+                current += recurse(l, i-1) + recurse(i+1, r)
+                cache[l][r] = max(cache[l][r], current)
+            return cache[l][r]
+        
+        return recurse(1, len(nums)-2)
+        
