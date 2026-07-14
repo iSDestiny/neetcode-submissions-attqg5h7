92e2562a140ffb8174cd9fa698b7157787ecfcb3
Jason Bugallon <jbugallo@uci.edu>
2026-07-14 10:43:22 +0900

    Add: merge-triplets-to-form-target - submission-3

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    # Time: O(n)
+    # Space: O(n)
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        ra,rb,rc = -1,-1,-1
+        for a,b,c in triplets:
+            if a > target[0] or b > target[1] or c > target[2]:
+                continue
+            ra,rb,rc = max(ra,a), max(rb,b), max(rc,c)
+        
+        return ra == target[0] and rb == target[1] and rc == target[2]
