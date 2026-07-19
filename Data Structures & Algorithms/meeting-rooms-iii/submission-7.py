82403d45820980aa5ebf04643ab80d18fa0d3333
Jason Bugallon <jbugallo@uci.edu>
2026-07-19 22:35:03 +0900

    Add: meeting-rooms-iii - submission-7

---
+++ Data Structures & Algorithms/meeting-rooms-iii/submission-7.py
@@ -0,0 +1,38 @@
+from heapq import heappush, heappop, heapify
+from collections import defaultdict
+
+class Solution:
+    def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
+        room_count = defaultdict(int)
+        available_room = list(range(n))
+        heapify(available_room)
+        heap = []
+
+        for start, end in sorted(meetings, key=lambda x: x[0]):
+            # print(start,end)
+            # print(heap)
+            while heap and heap[0][0] <= start:
+                _, free_room = heappop(heap)
+                heappush(available_room, free_room)
+            if len(heap) == n:
+                free_end, free_room = heappop(heap)
+                heappush(available_room, free_room)
+                delay = free_end - start
+                end = end + delay
+
+            next_room = heappop(available_room)
+            heappush(heap, (end, next_room))
+            room_count[next_room] += 1
+            # print(heap)
+            # print(start,end)
+            # print("=========")
+
+        
+        best_count = 0
+        best_room = 0
+        for room in range(n):
+            if room_count[room] > best_count:
+                best_count = room_count[room]
+                best_room = room
+        print(room_count)
+        return best_room
